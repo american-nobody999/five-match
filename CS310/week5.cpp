f3eb62f3b48826ca7f2b3ada9b9d2125c3cf2bcf
american-nobody999 <american_nobody999@proton.me>
2026-06-04 18:26:08 -0700

    Create week5.cpp
    bs github

---
+++ CS310/week5.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+ int a = 497;
+ int b = 496;
+ int result = a-b;
+int calculateDifference(int a, int b) {
+
+        return a - b
+    }
+
+//simple math problem, use the function instead of global variables//
+int main() {
+    std::cout << "We are coming back!" << calculateDifference(a, b) << std::endl;
+    return 0
+}
+
+//for future refernce all of errors will be syntax errors. I am sick of trying to be clever with them, and spending time trying to get it to push to the git.//
+
+
+//basic function structure//
+//returnType functionName(parameters) {
+//    // code here
+//    //return something//
+//}
